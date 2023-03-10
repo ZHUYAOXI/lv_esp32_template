e3411620e81de48bbfcf72b8beadee93c20df77d
ZHUYAOXI <2220840737@qq.com>
2023-03-10 09:13:51 +0800

    初始化仓库

---
+++ lvglinit.bat
@@ -0,0 +1,7 @@
+mkdir components
+
+cd .\components
+
+git clone https://gitee.com/dmcus/lvgl.git
+
+git clone https://gitee.com/exp-pi/lvgl_esp32_drivers.git
---
+++ main/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <inttypes.h>
+#include "sdkconfig.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+void app_main(void)
+{
+   while (1)
+   {
+      ;
+   }
+}
